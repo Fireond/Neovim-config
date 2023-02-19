d391585f3d7124bf9e18ea3572c6cd654f505ba5
hanyu_yan <1824831405@qq.com>
2023-02-19 23:23:52 +0800

    set timeoutlen = 200

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,6 +2,7 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+vim.opt.timeoutlen = 200
 -- ======================= --
 -- Set line wrap and break --
 -- ======================= --
