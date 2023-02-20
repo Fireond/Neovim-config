8237be6b779f7e4b642ab25041b8f6cf7c74c974
hanyu_yan <1824831405@qq.com>
2023-02-20 16:37:09 +0800

    update options.lua

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -16,13 +16,12 @@ vim.opt.showbreak = ">>"
 -- ============= --
 vim.opt.conceallevel = 2
 vim.g.vimtex_view_method = "skim"
--- Do not open pdfviwer on compile
 vim.g.vimtex_view_automatic = 0
--- Disable quickfix auto open
 vim.g.vimtex_quickfix_ignore_mode = 0
--- Do not auto open quickfix on compile erros
 vim.g.vimtex_quickfix_mode = 0
+vim.g.vimtex_compiler_silent = 1
 vim.g.vimtex_view_general_options = "-r @line @pdf @tex"
+vim.g.vimtex_indent_enabled = 0
 -- Custom conceasl
 vim.cmd([[
 let g:vimtex_syntax_custom_cmds = [
@@ -31,11 +30,19 @@ let g:vimtex_syntax_custom_cmds = [
   \ {'name': 'cp', 'mathmode': 1, 'concealchar': 'x'},
   \ {'name': 'rank', 'mathmode': 1, 'concealchar': 'r'},
   \ {'name': 'vb', 'mathmode': 1, 'conceal': 1, 'argstyle': 'boldunder'},
+  \ {'name': 'qty', 'mathmode': 1, 'conceal': 1},
   \]
+]])
+vim.cmd([[
+let g:vimtex_syntax_custom_cmds_with_concealed_delims = [
+          \ {'name': 'ket',
+          \  'mathmode': 1,
+          \  'conceal': 0,
+          \  'cchar_open': '|',
+          \  'cchar_close': '>'},
+          \]
 ]])
 
 -- =============== --
 -- Markdown Config --
 -- =============== --
-vim.g.mkdp_auto_start = 1
-vim.g.mkdp_auto_close = 1
