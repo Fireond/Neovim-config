7795743e5b9b02bb7c9aa16f005a0d390580a6a2
hanyu_yan <1824831405@qq.com>
2023-03-09 18:04:46 +0800

    update

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,12 +1,14 @@
--- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
-
 vim.opt.timeoutlen = 150
 vim.opt.spell = true
 vim.opt.spelllang = { "en", "cjk" }
 vim.opt.spelloptions = "camel"
-vim.opt.backup = 0
+vim.opt.backup = false
+
+-- Knap Config
+
+vim.g.knap_settings = {
+  delay = 500,
+}
 -- ============= --
 -- Vimtex Config --
 -- ============= --
@@ -41,8 +43,3 @@ vim.opt.backup = 0
 --   \]
 -- ]])
 --
--- Knap Config
-
-vim.g.knap_settings = {
-  delay = 500,
-}
